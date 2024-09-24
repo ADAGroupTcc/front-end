2557765fc77f66d0672570ac0b01df1562d5287c
Gabriel Lopes <gabriellopes.gl2805@gmail.com>
2024-09-24 07:23:31 -0300

    finished screen
    finalização do layout da primeira tela

--- lib/main.dart
+++ lib/main.dart
@@ -31,114 +31,159 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset(
-          'assets/registerbackground.png',
-          fit: BoxFit.cover,
-          height: double.infinity,
-          width: double.infinity,
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: 50),
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/logoadda.png',
-                  fit: BoxFit.fitWidth,
-                  width: 115,
-                  height: 180,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  "Bem vindo(a) ao ADDA!",
-                  style: TextStyle(
-                    fontSize: 58,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontFamily: 'Amaranth',
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  "Aqui você encontra estações de tripulantes que combinam com você, de forma altamente personalizável!",
-                  style: TextStyle(
-                    fontSize: 27,
-                    fontFamily: 'Inter',
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w300,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-              ),
-            ],
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/registerbackground.png',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Image.asset(
-                'assets/blackwave.png',
-                fit: BoxFit.fitWidth,
-                width: double.infinity,
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "Primeiros passos",
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: screenHeight * 0.07),
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/logoadda.png',
+                    fit: BoxFit.fitWidth,
+                    width: screenWidth * 0.3,
+                    height: screenHeight * 0.2,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                  child: Text(
+                    "Bem vindo(a) ao ADDA!",
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: screenWidth * 0.14,
+                      fontWeight: FontWeight.w600,
                       color: Colors.white,
-                      fontFamily: "Amaranth",
-                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Amaranth',
                       decoration: TextDecoration.none,
+                      height: 1,
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SegundaRota()),
-                      );
-                    },
-                    style: ButtonStyle(
-                        minimumSize:
-                            WidgetStateProperty.all(const Size(20, 10)),
-                        backgroundColor: WidgetStateProperty.all<Color>(branco),
-                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                side: const BorderSide(
-                                    color: branco, width: 2)))),
-                    child: const Text(
-                      "Crie uma conta",
-                      style: TextStyle(
-                        color: preto,
-                        fontFamily: "Amaranth",
-                        fontSize: 25,
+                ),
+                const SizedBox(height: 6),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                  child: Text(
+                    "Aqui você encontra estações de tripulantes que combinam com você, de forma altamente personalizável!",
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.059,
+                      fontFamily: 'Inter',
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w300,
+                      decoration: TextDecoration.none,
+                      height: 1.3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/blackwave.png',
+                  fit: BoxFit.fitWidth,
+                  width: double.infinity,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: screenWidth * 0.064),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Primeiros passos",
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.05,
+                            color: Colors.white,
+                            fontFamily: "Amaranth",
+                            fontWeight: FontWeight.normal,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      )
+                    ),
+                    const SizedBox(height: 12),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SegundaRota()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth * 0.24,
+                            vertical: screenHeight * 0.014),
+                        backgroundColor: branco,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: const BorderSide(color: branco, width: 4),
+                        ),
+                      ),
+                      child: Text(
+                        "Crie uma conta",
+                        style: TextStyle(
+                          color: preto,
+                          fontFamily: "Amaranth",
+                          fontSize: screenWidth * 0.06,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                    const SizedBox(height: 12),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SegundaRota()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth * 0.361,
+                            vertical: screenHeight * 0.014),
+                        backgroundColor: Colors.transparent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: const BorderSide(color: branco, width: 2),
+                        ),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: branco,
+                          fontFamily: "Amaranth",
+                          fontSize: screenWidth * 0.06,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
