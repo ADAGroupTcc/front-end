46926744ac77b22a03a19e7f43b1cd5ffeec0619
rafael-ferraz-s <rafael.ferraz@picpay.com>
2024-11-01 19:14:40 -0300

    feat: list cache channel

--- lib/sdk/LocalCache.dart
+++ lib/sdk/LocalCache.dart
@@ -3,6 +3,8 @@ import 'package:addaproject/sdk/model/User.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
+import 'model/Channel.dart';
+
 class LocalCache {
   Future<void> saveUserSession(User user) async {
     final prefs = await SharedPreferences.getInstance();
@@ -18,5 +20,22 @@ class LocalCache {
 
     final res = jsonDecode(jsonUser);
     return User.fromJson(res);
+  }
+
+  Future<List<Channel>?> listChannelCached() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonChannels = prefs.getString('channels');
+    if (jsonChannels == null) {
+      return null;
+    }
+
+    final res = jsonDecode(jsonChannels) as List<dynamic>;
+    return res.map((channel) => Channel.fromJson(channel)).toList();
+  }
+
+  Future<void> saveChannels(List<Channel> channels) async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonChannels = jsonEncode(channels.map((channel) => channel.toJson()).toList());
+    await prefs.setString('channels', jsonChannels);
   }
 }
